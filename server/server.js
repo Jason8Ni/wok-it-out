2979f031ec2c9128eccfc7937b37e84bd751b580
derek.liu <derek.liu@autodesk.com>
2018-11-24 19:29:43 -0500

    changes to cloud vision api

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override')
 var cors = require('cors');
- 
+// var multer = require('multer');
+// var path = require('path');
+var fs = require('fs');
+var Readable = require('stream').Readable
+
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,9 +21,16 @@ app.use(cors());
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
 
-app.get('/processImage', function(req, res) {
+app.get('/', function(req, res) {
+	res.send("Default testing response");
+})
+
+app.post('/uploadAndProcess', function(req, res) {
+	fs.writeFile("out.png", req.body.base64Img, 'base64', function(err) {
+	  console.log(err);
+	});
 	client
-		.labelDetection('../public/images/wok.jpg')
+		.labelDetection('./out.png')
 		.then(results => {
 			const labels = results[0].labelAnnotations
 			res.send(labels);
@@ -29,7 +40,7 @@ app.get('/processImage', function(req, res) {
 
 app.get('/queryRecipe', function(req, res) {
 	var ingredientArray = ["Apple", "Flour", "Onions"]//req.query.ingredients
-	var ingredientString = ingredientArray.join(" ");
+	var ingredientString = ingredientArray.join(",");
 	const url = "https://api.edamam.com/search?q=chicken&app_id=a2f3eb05&app_key=28465817b5ea90aaa8e5ce019f5f5d61";
 
 	request(url, function(error, response, body) {
